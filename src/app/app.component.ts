84bfcd584d4f9777e53d5de7057fa8ea2a682055
carlosdanielgr <carlos.dgr.12@gmail.com>
2024-09-30 15:28:01 -0500

    Main content created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MainContentComponent } from './components/main-content/main-content.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [MainContentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })

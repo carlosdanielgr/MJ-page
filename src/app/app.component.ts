5a90885d286bb8a4578e377f9e8f85f2141d6f8e
carlosdanielgr <carlos.dgr.12@gmail.com>
2024-10-01 13:51:48 -0500

    Circle-images component completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MainContentComponent } from './components/main-content/main-content.component';
+import { CircleImagesComponent } from './components/circle-images/circle-images.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [MainContentComponent],
+  imports: [MainContentComponent, CircleImagesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })

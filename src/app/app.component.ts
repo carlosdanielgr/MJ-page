be279f212f0571713b996dfe400a3cb5f0104660
carlosdanielgr <carlos.dgr.12@gmail.com>
2024-10-01 16:58:51 -0500

    About-me component completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { MainContentComponent } from './components/main-content/main-content.component';
 import { CircleImagesComponent } from './components/circle-images/circle-images.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [MainContentComponent, CircleImagesComponent],
+  imports: [MainContentComponent, CircleImagesComponent, AboutMeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })

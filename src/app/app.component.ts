1e4de0589317e3a661001e5909215e11173d83a2
carlosdanielgr <carlos.dgr.12@gmail.com>
2024-10-02 13:24:28 -0500

    course description completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { MainContentComponent } from './components/main-content/main-content.com
 import { CircleImagesComponent } from './components/circle-images/circle-images.component';
 import { AboutMeComponent } from './components/about-me/about-me.component';
 import { MyHistoryComponent } from './components/my-history/my-history.component';
+import { CourseDescriptionComponent } from './components/course-description/course-description.component';
 
 @Component({
   selector: 'app-root',
@@ -12,6 +13,7 @@ import { MyHistoryComponent } from './components/my-history/my-history.component
     CircleImagesComponent,
     AboutMeComponent,
     MyHistoryComponent,
+    CourseDescriptionComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',

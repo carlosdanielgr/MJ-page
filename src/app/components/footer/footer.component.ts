72a8f90fb636f1626bb1df62e159c6488cfec378
carlosdanielgr <carlos.dgr.12@gmail.com>
2024-10-04 16:59:04 -0500

    loading lazy added

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { API_WHATSAPP } from '../../global.constant';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [NgOptimizedImage],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
 })
